74f159a634617568f3a55542b91f962ae1d805c4
huangcygit <1013649574@qq.com>
2020-06-28 09:10:24 +0800

    netty

--- netty-st/src/main/java/com/hy/netty/first/NettyTimeServer.java
+++ netty-st/src/main/java/com/hy/netty/first/NettyTimeServer.java
@@ -19,9 +19,14 @@ public class NettyTimeServer {
             //创建ServerBootstrap，是netty用于启动NIO服务端的辅助启动类
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup,workerGroup)
+                    // 主线程处理类,看到这样的写法，底层就是用反射
                     .channel(NioServerSocketChannel.class)
+                    // 针对主线程的配置 分配线程最大数量 1024
                     .option(ChannelOption.SO_BACKLOG,1024)
-                    .childHandler(new ChildChannelHandler()); //绑定I/O的处理类为ChildChannelHandler
+                    // 针对子线程的配置 保持长连接
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    //子线程处理类 绑定I/O的处理类为ChildChannelHandler
+                    .childHandler(new ChildChannelHandler());
             //调用bind方法绑定监听端口，调用同步阻塞方法sync等待绑定操作的完成
             ChannelFuture f = b.bind(port).sync();
             //进行阻塞，等待服务端链路关闭之后才退出main函数
